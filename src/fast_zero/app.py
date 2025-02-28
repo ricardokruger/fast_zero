ef2a9f7c81d56cffdba2276000164297c9ad7a80
Ricardo Kruger <ricardokruger@gmail.com>
2025-02-28 15:03:05 -0300

    Adicionada autenticação e autorização com JWT

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,17 +1,24 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
-database = []
+# database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -39,8 +46,10 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
                 detail='Email already exists',
             )
 
+    hashed_password = get_password_hash(user.password)
+
     db_user = User(
-        username=user.username, password=user.password, email=user.email
+        username=user.username, password=hashed_password, email=user.email
     )
     session.add(db_user)
     session.commit()
@@ -75,17 +84,26 @@ def read_user(user_id: int, session: Session = Depends(get_session)):
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+    # db_user = session.scalar(select(User).where(User.id == user_id))
 
-    if not db_user:
-        raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
+    # if not db_user:
+    #     raise HTTPException(HTTPStatus.NOT_FOUND, detail='User not found')
+
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
+        )
+    db_user = current_user
 
     try:
         db_user.username = user.username
         db_user.email = user.email
-        db_user.password = user.password
+        db_user.password = get_password_hash(user.password)
         session.commit()
         session.refresh(db_user)
 
@@ -101,15 +119,48 @@ def update_user(
 @app.delete(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
 )
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    # db_user = session.scalar(select(User).where(User.id == user_id))
 
-    if not db_user:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+    # if not db_user:
+    #     raise HTTPException(
+    #         status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+    #     )
+    if current_user.id != user_id:
+        HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
+    db_user = current_user
 
     session.delete(db_user)
     session.commit()
 
     return {'message': 'User deleted'}
+
+
+@app.post('/token', status_code=HTTPStatus.OK, response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token({'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fast_zero.schemas import UserPublic
+from fast_zero.security import create_access_token
 
 
 def test_root_deve_ser_ok_e_ola_mundo(client):
@@ -87,9 +88,10 @@ def test_users_not_found(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
         '/users/1',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'NewUserName',
             'email': 'other@email.com',
@@ -105,7 +107,7 @@ def test_update_user(client, user):
     }
 
 
-def test_update_user_integrity_error(client, user):
+def test_update_user_integrity_error(client, user, token):
     # Criando um registro para "fausto"
     client.post(
         '/users',
@@ -119,6 +121,7 @@ def test_update_user_integrity_error(client, user):
     # Alterando o user.username das fixture para fausto
     response_update = client.put(
         f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'fausto',
             'email': 'bob@example.com',
@@ -132,29 +135,92 @@ def test_update_user_integrity_error(client, user):
     }
 
 
-def test_update_user_not_found(client):
-    response = client.put(
-        '/users/9999',
-        json={
-            'username': 'NewUserName',
-            'email': 'other@email.com',
-            'password': 'newpass987',
-        },
+# def test_update_user_not_found(client, token):
+#     response = client.put(
+#         '/users/9999',
+#         headers={'Authorization': f'Bearer {token}'},
+#         json={
+#             'username': 'NewUserName',
+#             'email': 'other@email.com',
+#             'password': 'newpass987',
+#         },
+#     )
+
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+# assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}', headers={'Authorization': f'Bearer {token}'}
     )
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_delete_user(client, user):
-    response = client.delete(f'/users/{user.id}')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_delete_user_not_found(client):
-    response = client.delete('/users/9999')
+# def test_delete_user_not_found(client, token):
+#     response = client.delete(
+#         '/users/9999', headers={'Authorization': f'Bearer {token}'}
+#     )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
+
+
+def test_get_token_invalid_user(client, user):
+    response = client.post(
+        '/token',
+        data={'username': 'no@exist', 'password': 'testtest'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_get_token_invalid_password(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': 'nopassword'},
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_get_sub_not_found_in_jwt(client):
+    data = {'no-email': 'test'}
+    token = create_access_token(data)
+
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validade credentials'}
+
+
+def test_get_current_user_does_not_exists(client):
+    data = {'sub': 'test@test'}
+    token = create_access_token(data)
+
+    response = client.delete(
+        '/users/1',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validade credentials'}
