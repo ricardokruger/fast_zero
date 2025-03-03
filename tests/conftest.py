9a1f27eed21c87010d928a52859d1a01cf6cc49e
Ricardo Kruger <ricardokruger@gmail.com>
2025-03-03 20:27:53 -0300

    Dockerizando nossa aplicação e inserindo o PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,15 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from sqlalchemy.pool import StaticPool
+from testcontainers.postgres import PostgresContainer
 
+# from sqlalchemy.pool import StaticPool
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 from fast_zero.security import get_password_hash
+
+# from fast_zero.settings import Settings
 
 
 class UserFactory(factory.Factory):
@@ -35,13 +38,23 @@ def client(session):
     app.dependency_overrides.clear()
 
 
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        _engine = create_engine(postgres.get_connection_url())
+
+        with _engine.begin():
+            yield _engine
+
+
 @pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+def session(engine):
+    # engine = create_engine(
+    #     'sqlite:///:memory:',
+    #     connect_args={'check_same_thread': False},
+    #     poolclass=StaticPool,
+    # )
+
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
